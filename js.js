8982b533f3c0c21268ef619f4f167029a88d3d79
Alexey Yavnyk <thesorbex@gmail.com>
2016-12-08 14:58:25 +0200

    add PopUp

--- js.js
+++ js.js
@@ -37,3 +37,28 @@ function closeAndOpen(elem){
 		}
 	}
 }
+
+function openPopUp(){
+	var a = document.getElementById("PopUp");
+	a.style.display = "block";
+	a.style.height = "100%";
+	a.style.width = "100%";
+	a.style.zIndex = 4;
+	a.style.position = "fixed";
+	a.style.backgroundColor = "rgba(0,0,0,0.5)";
+
+	var b = document.getElementById("childPopUp");
+	b.style.width = '40%';
+	b.style.height = '40%';
+	b.style.backgroundColor = "white";
+	b.style.display = "block";
+	b.style.marginLeft = "auto"
+	b.style.marginRight = "auto"
+	b.style.marginTop = "300px"
+
+}
+
+function closePopUp(){
+	var a = document.getElementById("PopUp");
+	a.style.display = "none";	
+}
